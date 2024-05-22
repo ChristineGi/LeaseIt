91b50da284d2604c276fc5b695b425829b4ba4c9
ChristineGi <christinegianneli2002@gmail.com>
2024-05-22 12:58:20 +0300

    COMMIT

---
+++ Code/LeaseIT/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Example of using the new method
+        int a = 5;
+        int b = 10;
+        System.out.println("Sum: " + add(a, b));
+    }
+
+    // A public method to add two integers
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
